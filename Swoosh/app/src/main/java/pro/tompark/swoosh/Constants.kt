9d501b81724a71240e1841e2d1c84535c6208b1e
tomparkpro <tom.hyunung.park@gmail.com>
2018-05-03 02:23:13 +0900

    add string data into Intent

---
+++ Swoosh/app/src/main/java/pro/tompark/swoosh/Constants.kt
@@ -0,0 +1,3 @@
+package pro.tompark.swoosh
+
+const val EXTRA_LEAGUE = "league"
---
+++ Swoosh/app/src/main/java/pro/tompark/swoosh/SkillActivity.kt
@@ -0,0 +1,16 @@
+package pro.tompark.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : AppCompatActivity() {
+
+    var league = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+        league = intent.getStringExtra(EXTRA_LEAGUE)
+        println(league)
+    }
+}
