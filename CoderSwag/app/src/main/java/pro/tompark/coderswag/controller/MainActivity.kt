759f3d5ef0c18c35f70ef637f5a110006eb37df5
tomparkpro <tom.hyunung.park@gmail.com>
2018-05-06 14:11:10 +0900

    add ListView Click Toast

--- CoderSwag/app/src/main/java/pro/tompark/coderswag/controller/MainActivity.kt
+++ CoderSwag/app/src/main/java/pro/tompark/coderswag/controller/MainActivity.kt
@@ -2,6 +2,7 @@ package pro.tompark.coderswag.controller
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import pro.tompark.coderswag.R
 import pro.tompark.coderswag.adapters.CategoryAdapter
@@ -18,5 +19,10 @@ class MainActivity : AppCompatActivity() {
         adapter = CategoryAdapter(this, DataService.categories)
 
         catetoryListView.adapter = adapter
+
+//        catetoryListView.setOnItemClickListener { parent, view, position, id ->
+//            val category = DataService.categories[position]
+//            Toast.makeText(this, "You clicked on the ${category.title} cell", Toast.LENGTH_SHORT).show()
+//        }
     }
 }
