415153e7f4ab8df4bdea8a3aa7b943ecf272352a
tomparkpro <tom.hyunung.park@gmail.com>
2018-05-07 23:19:25 +0900

    add CreateUserActivity UI

---
+++ Smack/app/src/main/java/pro/tompark/smack/LoginActivity.kt
@@ -0,0 +1,23 @@
+package pro.tompark.smack
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginLoginBtnClicked(view: View) {
+
+    }
+
+    fun loginCreateUserBtnClicked(view: View) {
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
+    }
+}
