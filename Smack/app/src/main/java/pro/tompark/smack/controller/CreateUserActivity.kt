fda45f0b96e191150dd4a485f31e30001bec6b06
tomparkpro <tom.hyunung.park@gmail.com>
2018-05-10 02:36:38 +0900

    add Login User funtionality

--- Smack/app/src/main/java/pro/tompark/smack/controller/CreateUserActivity.kt
+++ Smack/app/src/main/java/pro/tompark/smack/controller/CreateUserActivity.kt
@@ -50,9 +50,16 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View) {
-        AuthService.registerUser(this, "tom@tompark.pro", "123456") { complete ->
-            if (complete) {
-
+        val email = createEmailText.text.toString()
+        val password = createPasswordText.text.toString()
+        AuthService.registerUser(this, email, password) { registerSuccess ->
+            if (registerSuccess) {
+                AuthService.loginUser(this, email, password) { loginSuccess ->
+                    if (loginSuccess) {
+                        println(AuthService.authToken)
+                        println(AuthService.userEmail)
+                    }
+                }
             }
         }
     }
