367ec7ae5038827137f5a443d34c675d843609dd
tomparkpro <tom.hyunung.park@gmail.com>
2018-05-11 02:04:58 +0900

    add Local Broadcast Receiver

--- Smack/app/src/main/java/pro/tompark/smack/controller/MainActivity.kt
+++ Smack/app/src/main/java/pro/tompark/smack/controller/MainActivity.kt
@@ -1,14 +1,23 @@
 package pro.tompark.smack.controller
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 import pro.tompark.smack.R
+import pro.tompark.smack.services.AuthService
+import pro.tompark.smack.services.UserDataService
+import pro.tompark.smack.utilities.BROADCAST_USER_DATA_CHANGE
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +31,20 @@ class MainActivity : AppCompatActivity() {
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
+        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver, IntentFilter(BROADCAST_USER_DATA_CHANGE))
+    }
+
+    private val userDataChangeReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (AuthService.isLoggedIn) {
+                userNameNavHeader.text = UserDataService.name
+                userEmailNavHeader.text = UserDataService.email
+                val resourceId = resources.getIdentifier(UserDataService.avatarName, "drawable", packageName)
+                userImageNavHeader.setImageResource(resourceId)
+                userImageNavHeader.setBackgroundColor(UserDataService.returnAvatarColor(UserDataService.avatarColor))
+                loginBtnNavHeader.text = "Logout"
+            }
+        }
     }
 
     override fun onBackPressed() {
@@ -33,8 +56,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked(view: View) {
-        val loginIntent = Intent(this, LoginActivity::class.java)
-        startActivity(loginIntent)
+        if (AuthService.isLoggedIn) {
+            // log out
+            UserDataService.logout()
+            userNameNavHeader.text = "Login"
+            userEmailNavHeader.text = ""
+            userImageNavHeader.setImageResource(R.drawable.profiledefault)
+            userImageNavHeader.setBackgroundColor(Color.TRANSPARENT)
+            loginBtnNavHeader.text = "Login"
+        } else {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
     }
 
     fun addChannelClicked(view: View) {
--- Smack/app/src/main/java/pro/tompark/smack/services/UserDataService.kt
+++ Smack/app/src/main/java/pro/tompark/smack/services/UserDataService.kt
@@ -1,5 +1,8 @@
 package pro.tompark.smack.services
 
+import android.graphics.Color
+import java.util.*
+
 /**
  * Created by TomPark on 2018. 5. 11.
  *
@@ -14,4 +17,39 @@ object UserDataService {
     var avatarName = ""
     var email = ""
     var name = ""
+
+    fun logout() {
+        id = ""
+        avatarColor = ""
+        avatarName = ""
+        email = ""
+        name = ""
+        AuthService.authToken = ""
+        AuthService.userEmail = ""
+        AuthService.isLoggedIn = false
+    }
+
+    fun returnAvatarColor(components: String) : Int {
+        // [0.26666666666666666, 0.7215686274509804, 0.4, 1]
+        // 0.26666666666666666 0.7215686274509804 0.4 1
+
+        val strippedColor = components
+                .replace("[", "")
+                .replace("]", "")
+                .replace(",", "")
+
+        var r = 0
+        var g = 0
+        var b = 0
+
+        val scanner = Scanner(strippedColor)
+        if (scanner.hasNext()) {
+            r = (scanner.nextDouble() * 255).toInt()
+            g = (scanner.nextDouble() * 255).toInt()
+            b = (scanner.nextDouble() * 255).toInt()
+        }
+
+        return Color.rgb(r, g, b)
+    }
+
 }
