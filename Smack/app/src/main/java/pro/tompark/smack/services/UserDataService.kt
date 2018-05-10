db4f6f190a016926eed8f7d5099a1d998190cab1
tomparkpro <tom.hyunung.park@gmail.com>
2018-05-11 01:39:48 +0900

    add Create User functionality

---
+++ Smack/app/src/main/java/pro/tompark/smack/services/UserDataService.kt
@@ -0,0 +1,17 @@
+package pro.tompark.smack.services
+
+/**
+ * Created by TomPark on 2018. 5. 11.
+ *
+ * @author tom.hyunung.park@gmail.com
+ * github : http://github.com/tomparkpro
+ *
+ */
+object UserDataService {
+
+    var id = ""
+    var avatarColor = ""
+    var avatarName = ""
+    var email = ""
+    var name = ""
+}
--- Smack/app/src/main/java/pro/tompark/smack/utilities/Constants.kt
+++ Smack/app/src/main/java/pro/tompark/smack/utilities/Constants.kt
@@ -7,8 +7,11 @@ package pro.tompark.smack.utilities
  * github : http://github.com/tomparkpro
  *
  */
-//const val BASE_URL = "https://tomparkchat.herokuapp.com/v1/"
-const val BASE_URL = "http://10.0.2.2:3005/v1/"
+const val BASE_URL = "https://tomparkchat.herokuapp.com/v1/"
+//const val BASE_URL = "http://10.0.2.2:3005/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
+const val URL_CREATE_USER = "${BASE_URL}user/add"
 
+// Broadcast Constant
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
