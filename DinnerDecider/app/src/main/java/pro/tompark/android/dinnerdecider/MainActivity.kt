e72a95387e876e108d1688c038abd676c7dcc8fd
tomparkpro <tom.hyunung.park@gmail.com>
2018-05-17 00:35:10 +0900

    add Dinner Decider App for PlayStore Upload test

--- DinnerDecider/app/src/main/java/pro/tompark/android/dinnerdecider/MainActivity.kt
+++ DinnerDecider/app/src/main/java/pro/tompark/android/dinnerdecider/MainActivity.kt
@@ -2,6 +2,7 @@ package pro.tompark.android.dinnerdecider
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -25,7 +26,7 @@ class MainActivity : AppCompatActivity() {
             var newFood = addFoodTxt.text.toString()
             foodList.add(newFood)
             addFoodTxt.text.clear()
-            println(foodList)
+            Log.d("TAG", "This is a test log")
         }
     }
 }
